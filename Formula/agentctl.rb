9a3ea3e59d66ed6936028e119124ed013eaa1784
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2026-01-28 23:24:02 -0800

    chore: add agentctl formula

---
+++ Formula/agentctl.rb
@@ -0,0 +1,36 @@
+class Agentctl < Formula
+  desc "CLI for managing Agents through Kubernetes or Jangar gRPC"
+  homepage "https://github.com/proompteng/lab/tree/main/services/jangar/agentctl"
+  version "0.1.0"
+  license "MIT"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/proompteng/lab/releases/download/agentctl-v#{version}/agentctl-#{version}-darwin-arm64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+    on_intel do
+      url "https://github.com/proompteng/lab/releases/download/agentctl-v#{version}/agentctl-#{version}-darwin-amd64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/proompteng/lab/releases/download/agentctl-v#{version}/agentctl-#{version}-linux-arm64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+    on_intel do
+      url "https://github.com/proompteng/lab/releases/download/agentctl-v#{version}/agentctl-#{version}-linux-amd64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  def install
+    bin.install "agentctl"
+  end
+
+  test do
+    system bin/"agentctl", "version", "--client"
+  end
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+# proompteng Homebrew Tap
+
+Install:
+
+```bash
+brew tap proompteng/tap
+brew install proompteng/tap/agentctl
+```
+
+`agentctl` binaries are published from `proompteng/lab` releases tagged `agentctl-vX.Y.Z`.
