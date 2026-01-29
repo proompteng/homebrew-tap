e25c12610cecea4b99f65cce07ba56ac3cfa6713
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2026-01-29 00:23:35 -0800

    chore: update agentctl to 0.1.0

--- Formula/agentctl.rb
+++ Formula/agentctl.rb
@@ -7,22 +7,22 @@ class Agentctl < Formula
   on_macos do
     on_arm do
       url "https://github.com/proompteng/lab/releases/download/agentctl-v#{version}/agentctl-#{version}-darwin-arm64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "9b085616cb1abd8175dfd56d04e589b791020b44bfd3354ffbdf529d59def915"
     end
     on_intel do
       url "https://github.com/proompteng/lab/releases/download/agentctl-v#{version}/agentctl-#{version}-darwin-amd64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "6a3ce5e6c0c4fe0369ce04e42098a7a70ea86714bbf4145453b17c9171f82d09"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/proompteng/lab/releases/download/agentctl-v#{version}/agentctl-#{version}-linux-arm64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "1d997f2faeb134cf78d70557c493af5b23a7a0c2d7413d3557d7d324f5a8dc2c"
     end
     on_intel do
       url "https://github.com/proompteng/lab/releases/download/agentctl-v#{version}/agentctl-#{version}-linux-amd64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "e2d70994f71f9066ebe6f278c1d4df2b4d39bbfa8c5174fcddf6d2304c2ad344"
     end
   end
 
